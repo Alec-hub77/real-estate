422e8d137de023ba99ee98d8678ba2893636294e
Alec-hub77 <oleg.ostapiuk@yahoo.com>
2022-04-20 17:53:37 +0300

    suspense, lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from 'react';
+import { Suspense, lazy } from 'react';
 import './App.scss';
 
-import { fetchApi } from './utils/fetchApi';
-
-import { Banner, Property } from './components';
 import Error404 from './components/404/Error404';
 import { Routes, Route } from 'react-router-dom';
 
-import PropertyPage from './pages/propertyPage/PropertyPage';
-import MainPage from './pages/main/MainPage';
+
+const PropertyPage = lazy(() => import('./pages/propertyPage/PropertyPage'))
+const MainPage = lazy(() => import('./pages/main/MainPage'))
 
 
 function App() {
   
   return (
+    <Suspense fallback={<center>Loading...</center>}>
     <div className="app">
         <Routes>
-        <Route path='/' element={<MainPage />} />
+        <Route index element={<MainPage />} />
         <Route path="property/:id" element={<PropertyPage/>} />
         <Route path='*' element={<Error404/>} />
       </Routes>
     </div>
+    </Suspense>
   );
 }
 
